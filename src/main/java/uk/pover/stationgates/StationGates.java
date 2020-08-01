a0208509051150c05e739813683a9a81bb48625b
Blitheness <blitheness@oneiric.co.uk>
2020-08-01 18:19:21 +0100

    Create properties file containing strings to help future localisation

--- src/main/java/uk/pover/stationgates/StationGates.java
+++ src/main/java/uk/pover/stationgates/StationGates.java
@@ -2,13 +2,19 @@ package uk.pover.stationgates;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ResourceBundle;
+
+/**
+ * @author CoffeeAndChill (dev@oneiric.co.uk)
+ */
 public class StationGates extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        messages = ResourceBundle.getBundle("Messages");
     }
     @Override
     public void onDisable() {
 
     }
+    protected ResourceBundle messages;
 }
